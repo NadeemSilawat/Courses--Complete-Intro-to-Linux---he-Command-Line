ff982803a5cfcb50e47fe56c2b65060d57a32551
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-02-19 17:20:12 +0530

    anatomy-of-the-command-line-interface

---
+++ 01-anatomy-of-the-command-line-interface.js
@@ -0,0 +1,43 @@
+// ========== Command Line ==========// 
+
+// pwd
+
+// pwd
+
+// pwd--help
+
+// clear
+
+// // ------------------------
+
+// cd / home
+
+// pwd
+
+// cd / home / nadeem
+
+// cd..
+
+// pwd
+
+// cd / home / nadeem
+
+// cd../..
+
+// pwd
+
+// pwd
+
+// pwd 
+
+// cd home
+
+// cd nadeem
+
+// cd..
+
+// cd../ nadeem
+
+// cd../ nadeem /../ nadeem
+
+// cd../ nadeem /../ nadeem
