3ef492d192fd037a1393ccebafe5a5b03903b2af
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-02-20 22:22:29 +0530

    THE CLI

--- 01-the-cli.js
+++ 01-the-cli.js
@@ -1,4 +1,4 @@
-// ========== Command Line ==========// 
+// ========== Anatomy of the command line Interface ==========// 
 
 // pwd
 
@@ -41,3 +41,166 @@
 // cd../ nadeem /../ nadeem
 
 // cd../ nadeem /../ nadeem
+
+/*********************************************/
+    /*            CLI Directories & Arguments               */
+
+ 
+// ls
+
+// cd .. 
+// pwd
+
+// ls
+
+
+// pwd
+
+// ls home
+
+// ls --help
+
+// ---- Usage: ls [OPTION]... [FILE]...
+// 
+// clear
+
+
+
+/************************************/
+// echo
+
+// echo hy  //"hy" is a parameter
+
+//echo Welcome to frontend master
+
+// echo "Welcome to frontend master"
+
+
+// which ls 
+
+//cd /bin /ls 
+
+//cd /bin       //-bin directories
+//ls
+
+//cd /home 
+//pwd           //--/home
+
+//clear
+
+/****************************************/
+
+    /*            Flags               */
+
+// pwd --help
+
+// clear
+
+// ---------
+
+// echo --help
+
+// man echo
+
+// echo --help
+
+// clear
+
+
+//----------------
+
+// /home$ ls 
+
+// /home$ ls -l
+
+// /home$ ls -l
+
+// /home$ ls -l -a
+
+// /home$ touch .hidden-file
+
+// /home$ cd nadeem/
+
+// /home$ touch .hidden-file
+
+// /home$ ls -l -a
+
+// /home$ ls -l
+
+// /home$ clear
+
+
+//  -------------\\
+
+// /home$ ls -h
+
+// /home$ ls -- help
+
+// /home$ ls -- all
+
+// /home$ ls -- all -a
+
+// /home$ ls -a
+
+// /home$ ls -l -a
+
+// /home$ ls -al
+
+// /home$ ls -la
+
+// /home$ ls --all -l
+
+// clear
+
+// -----------------------
+   
+// :~$ ls
+
+// :~$ ls --ignore=snap
+
+// :~$ ls --ignore snape
+// :~$ ls --ignore snap
+
+// :~$ ls --ignore=snap --ignore=Home 
+
+// :~$ clear
+
+// ----------------------------
+
+// :~$ ls -lsah
+
+// :~$ ls --help
+
+// :~$ clear 
+
+// --------------------
+
+
+// :~$ ls --ignore snap
+
+// :~$ ls -I snap
+
+// :~$ ls --help 
+
+// :~$ clear
+
+// --------------------
+
+
+// :~$ pwd 
+
+// :~$ ls snap/
+
+// :~$ ls -lsah snap
+
+// :~$ ls snap -lash 
+
+// :~$ ls -lash snap
+
+// :~$ 
+
+
+
+
+
+
