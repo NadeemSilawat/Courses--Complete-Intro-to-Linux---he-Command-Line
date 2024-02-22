d8d7e7361bafac1567d98cec9e38062960c0308f
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-02-22 17:44:44 +0530

    Editor

--- 02-editor.js
+++ 02-editor.js
@@ -3,3 +3,69 @@
     //-------nano--------//
 
 
+ // :~$  kill -l
+
+ // :~$  cd ~
+
+  // :~$  nano textfile.txt    -----> GUN nano 2.9.3      textfile.txt
+
+
+
+
+// -- this is a textt file
+
+// -- I can write whatever I want
+
+// -- this is so cool
+
+
+//_------------------------
+
+    //-------Vim : A Beif History----------//
+
+//ed ed.text 
+/**
+ * output: ed.text: NO such file or directory
+ * hello
+ * ?
+ * MY name is Dom
+ * ?
+ * ^c
+ * 
+ */// press CTL+D exit
+
+
+ //ls
+
+ //ls -lsah
+
+ //clear
+ 
+ //-------------------------- 
+
+//-----Vim :Basic Command--------------------
+
+//  vim textfile.txt 
+
+/*Output:
+Press ENTER to continue
+
+this is brian
+
+we're typing in cim now 
+
+this is so much fun 
+
+write some more stuff
+
+ */
+
+// :w. That's how you save
+
+// :q! Quit
+
+// z:q may not work
+
+ // wq! This saves and quits
+
+// q! This just quits without saving
