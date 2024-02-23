159b6656b429800ea6dcc6994ecf8c9545e7cb51
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-02-23 15:49:14 +0530

    FILE,PIPES & PERMISSION

---
+++ 03-file-pipes-&-permissions.js
@@ -0,0 +1,33 @@
+// --------Reading Files--------- //
+
+// :~$ less textfile.txt
+
+// :~$ ls 
+// textfile.txt  test1.c   test2.c   test3.cpp
+
+// :~$  vi textfile.txt
+
+// :~$ less 
+
+// :~$ less textfile.txt
+
+// :~$ more textfile.txt
+
+
+//--------------------------------
+
+// :~$ man less     // --->manual for less.
+
+// :~$ less --help      //-->summary of less Commmand
+
+// :~$ man python3
+
+//--------------------------------
+
+
+// :~$ cat textfile.txt              // cat does is read the entire file and output it
+
+
+
+
+
