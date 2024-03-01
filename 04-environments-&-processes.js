d0ab14d45d899e27cf776814bc4911404789d714
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-03-01 21:56:38 +0530

    Environment & Processes

--- 04-environments-&-processes.js
+++ 04-environments-&-processes.js
@@ -68,6 +68,8 @@
 
 // :~$ ps
 
+// :~$ kill -9 <sleep-process-id>      //kills a specific process, identified by
+
 // :~$ ps
 
 // :~$ sleep 100 &                  
@@ -88,11 +90,117 @@
 
 // :~$ bg 1                 
 
-// :~$  
+// :~$ fg 1             //CTRL+G End
+
+// :~$ jobs 1
+
+// :~$ bg 1
+
+// :~$ jobs --help      
+
+// :~$ jobs -l          ---------> list process IDs in added
+
+// :~$ kill -9 1793
+
+// :~$ jobs 
+
+// :~$ sleep 1000 & 
+
+// :~$ jobs
+
+// :~$ sleep 1000 > output.txt &
+
+// :~$ jobs --help
+
+// :~$ jobs -l 
+
+// :~$ kill -9 1796 1797
+
+// :~$ job -l
+
+                        //------------Exit Codes & Process--------------//
+
+
+// :~$ date                  ---------> Curretn Date 
+
+// :~$ echo &?                --------> $? corresponds to the last exit code, in this case 0
+ 
+// :~$ yes                    -------> hit CTRL+C to stop it
+
+// :~$ echo $?               -------->  you stopped it so it exited with a non-zero code, 130
+
+            /*
+                 any number from 0 to 256.
+
+                0: means it was successful. Anything other than 0 means it failed
+                1: a good general catch-all "there was an error"
+                2: a bash internal error, meaning you or the program tried to use bash in an incorrect way
+                126: Either you don't have permission or the file isn't executable
+                127: Command not found
+                128: The exit command itself had a problem, usually that you provided a non-integer exit code to it
+                130: You ended the program with CTRL+C
+                137: You ended the program with SIGKILL
+                255: Out-of-bounds, you tried to exit with a code larger than 255
+            */
+
+// :~$ touch status.txt && date >> status.txt && uptime >> status.txt
+
+// :~$ cat status.txt
+            // current Date Time
+
+// :~$ uptime
+
+// :~$ false
+
+// :~$ echo &?
+        //221
+
+// :~$ true 
+
+// :~$ echo &?
+        //232
+
+// :~$ false && echo hi
+
+// :~$ true && echo hi              // hi
+
+// :~$ false && echo hi             // hi
+
+// :~$ true && echo hi
+
+// :~$ true ; false ; echo hi       // hi
+
+
+                //----------Subcommands----------//
+
+// :~$ echo I think $(whoami) is a really cool user
+            /*output : I think dell is a really cool user*/
+
+// :~$ echo the current date is &(date)
+
+// :~$ echo $(date +%x) - $(uptime) >> log.txt
+
+// :~$ cat log.txt
+
+// :~$ echo hi $(cat < log.txt) User
+
+// :~$ clear
 
 
 
-// :~$ kill -9 <sleep-process-id>      //kills a specific process, identified by
+
+
+
+
+// :~$ date && cat not-real-file.txt && echo hi         ---------->  the date will display but hi won't
+
+
+
+
+
+
+
+
 
 
 
