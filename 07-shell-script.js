1796836b6eaabf188de5652d783b6d763d526019
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-03-04 16:17:07 +0530

    Shell Script

--- 07-shell-script.js
+++ 07-shell-script.js
@@ -28,4 +28,76 @@
 
                     //---------Hashbang-----------//
 
-                    
+// :~$ vi genfile.sh
+    /**
+     * #!/bin/bash
+
+        mkdir -p ~/temp
+        cd ~/temp
+        touch file{1..10}.txt
+        echo done
+
+     * **/
+
+
+// :~$ chmod +x gen_files                    
+
+// :~$ ls -lsah
+
+// :~$ ./genfile        -----> done
+
+//----------------------------
+
+
+// :~$ which node                      --> /usr/local/bin/node
+
+// :~$  vi node_test
+        /**
+         *#! /snap/bin/node
+            console.log('this is running from node.js')
+         *  **/
+
+// :~$ chmod +x node_test
+
+// :~$ ./node_test               ----> this is running from node.js
+
+
+                //----------Path & Variable----------------//
+
+// :~$ ls /user/local/sbin
+// :~$ ls /user/local/games
+// :~$ ls /snap/bin
+
+// :~$ mkdir my_bin
+
+// :~$ m node_test my_bin
+
+// :~$ mv genfile my_bin
+
+// :~$ cd my_bin/
+
+// :~/my_bin$ ls
+
+// :~/my_bin$ cd ..
+
+// :~$ vi .bashrc   
+    // # added by me for testing purpose
+    // PATH=$PATH:/home/mohit/my_bin
+
+ 
+
+
+
+
+
+
+
+            
+
+
+
+
+
+
+
+          
