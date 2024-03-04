c53a38abb9dd8a90e0acc290bd300ab1285f5d3a
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-03-04 15:42:19 +0530

    Shell Script

---
+++ 07-shell-script.js
@@ -0,0 +1,31 @@
+        //-------Writing Script--------//
+
+// :~$ ls
+
+// :~$ vi genfile.sh
+            /***
+             * mkdir -p ~/temp
+                cd ~/temp
+                touch file{1..10}.txt
+                echo done
+
+             * ***/
+// :~$ source genfile.sh
+    // done
+
+// :~/temp$ ls
+
+// :~/temp$ cd ..
+
+// :~$ .genfile.sh  
+// done
+
+// :~/temp$ cd ..
+
+// :~$ bash gen_file.sh
+//done
+
+
+                    //---------Hashbang-----------//
+
+                    
